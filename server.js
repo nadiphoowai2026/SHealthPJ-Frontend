81abae1a096592d6712c47665209ad0340344f71
nadiphoowai2026 <nadiphoowai9@gmail.com>
2026-08-30 22:24:42 +0630

    Update console log message for frontend server

--- server.js
+++ server.js
@@ -8,5 +8,5 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(__dirname));
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Frontend running on port ${PORT}`);
+  console.log(`SHealthPJ Frontend running on port ${PORT}`);
 });
