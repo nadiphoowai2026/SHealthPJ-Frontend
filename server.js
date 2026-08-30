9e3acefd68c7b02d7d61abf3ae91d4162bbf76ce
nadiphoowai2026 <nadiphoowai9@gmail.com>
2026-08-30 22:37:00 +0630

    Serve home.html on root route

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express");
-const path = require("path");
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/home.html");
+});
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`SHealthPJ Frontend running on port ${PORT}`);
