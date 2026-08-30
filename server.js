ae49c146735181a8764ec7bc573cecefb6d62186
nadiphoowai2026 <nadiphoowai9@gmail.com>
2026-08-30 21:58:52 +0630

    Initialize Express server in server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(__dirname));
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Frontend running on port ${PORT}`);
+});
