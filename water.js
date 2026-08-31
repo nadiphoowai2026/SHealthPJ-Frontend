9b6a9dc91cf9c9648d16706d40d3552b1acf9277
nadiphoowai2026 <nadiphoowai9@gmail.com>
2026-08-31 11:24:48 +0630

    Update API URL and refactor water tracking logic

--- water.js
+++ water.js
@@ -2,7 +2,7 @@
 // WATER TRACKER
 // ========================================
 
-const API_URL = "https://smart-healthcare-backend-production-8601.up.railway.app";
+const API_URL = "https://smart-healthcare-backend-production-d51b.up.railway.app";
 
 
 // ========================================
